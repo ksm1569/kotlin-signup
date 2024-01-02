a488079d71da57acc077e50d01ecc81874def0e2
ksm1569 <tnals1564@naver.com>
2024-01-02 22:45:29 +0900

    셋팅

---
+++ src/main/kotlin/com/smsoft/kotlinsignup/KotlinSignupApplication.kt
@@ -0,0 +1,11 @@
+package com.smsoft.kotlinsignup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSignupApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSignupApplication>(*args)
+}
